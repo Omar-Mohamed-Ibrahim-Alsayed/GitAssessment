35e98a3fc3afb9cd84a456c70895570da1e9fab5
Omar-Mohamed-Ibrahim-Alsayed <93681796+Omar-Mohamed-Ibrahim-Alsayed@users.noreply.github.com>
2022-07-30 21:31:36 +0200

    Ver 1.0 (integers only)

---
+++ src/First.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(){
+    println("Please enter two numbers")
+    val reader = Scanner(System.`in`)
+    val x : Int= reader.nextInt()
+    println("+")
+    val y : Int= reader.nextInt()
+    println(x + y)
+}
